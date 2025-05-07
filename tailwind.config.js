2584189c9b57cf2e1c374b7f022e1846f5365f50
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-05-06 22:21:30 -0500

    installed tailwind and flowbite plugin

---
+++ tailwind.config.js
@@ -0,0 +1,77 @@
+ // tailwind.config.js
+ const flowbite = require('flowbite/plugin');
+ const forms = require('@tailwindcss/forms');
+
+ 
+ module.exports = {
+     darkMode: 'false',
+   content: [
+    "./Views/**/*.php",         // ✅ all PHP view files
+    "./Views/_partials/**/*.php", // ✅ partials too
+    "./index.php",               // ✅ root index.php
+    "./src/**/*.{html,js}",       // ✅ if you have test HTML or JS files in src
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-datepicker/**/*.js"
+   ],
+   theme: {
+     extend: {
+       colors: {
+         'primary-tan': '#ffefe7',
+         'primary-light-tan': '#fff7f3',
+         'primary-purple': '#534B6F',
+       }
+     }
+   },
+   plugins: [
+     forms,
+     flowbite,
+     flowbiteDatepicker
+   ],
+ }
+ 
+ tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+      }
+    },
+    fontFamily: {
+      'body': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ],
+      'sans': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ]
+    }
+  }
+}
